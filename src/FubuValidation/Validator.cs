7c6582590e1e725a16e98dce8ec7db893c15cedc
Brandon Behrens <brandonbehrens@gmail.com>
2011-09-28 17:01:39 -0500

    Revert "Removed static validation methods as they seem to be for testing and do an end run around the container state"
    This reverts commit 1e194e4e962e2a2656d4c0ea6c2af1892509ef06.

--- src/FubuValidation/Validator.cs
+++ src/FubuValidation/Validator.cs
@@ -38,5 +38,24 @@ namespace FubuValidation
             _sources.SelectMany(x => x.RulesFor(validatedType))
                 .Each(rule => rule.Validate(context));
         }
+
+        public static IValidator BasicValidator()
+        {
+            return new Validator(new TypeResolver(), new IValidationSource[]{
+                new FieldRuleSource(new FieldRulesRegistry(new IFieldValidationSource[0], new TypeDescriptorCache()))
+            });
+        }
+
+        public static Notification ValidateObject(object target)
+        {
+            return BasicValidator().Validate(target);
+        }
+
+        public static IEnumerable<NotificationMessage> ValidateField(object target, string propertyName)
+        {
+            var notification = ValidateObject(target);
+            return notification.AllMessages.Where(x => x.Accessors.Any(a => a.Name == propertyName));
+        }
+            
     }
 }

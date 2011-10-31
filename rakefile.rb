55b6de2216fc22a2fc883f6eb117a8cf0cb7056c
jmarnold <rncodepoet@gmail.com>
2011-10-31 00:19:35 -0500

    NuGet now includes structuremap extensions for fubuvalidation

--- rakefile.rb
+++ rakefile.rb
@@ -73,7 +73,7 @@ end
 desc "Compiles the app"
 task :compile => [:restore_if_missing, :clean, :version] do
   MSBuildRunner.compile :compilemode => COMPILE_TARGET, :solutionfile => 'src/FubuValidation.sln', :clrversion => CLR_TOOLS_VERSION
-  copyOutputFiles "src/FubuValidation/bin/#{COMPILE_TARGET}", "Fubu*.{dll,pdb}", props[:stage]  
+  copyOutputFiles "src/FubuValidation.StructureMap/bin/#{COMPILE_TARGET}", "Fubu*.{dll,pdb}", props[:stage]  
   copyOutputFiles "src/FubuMVC.Validation/bin", "FubuMVC.Validation.{dll,pdb}", props[:stage]  
 end
 

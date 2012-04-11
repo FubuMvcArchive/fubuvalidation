2d2cce6184c92b9ed704f438910bdc252df9ddaf
Jeremy Miller <jeremydmiller@yahoo.com>
2012-04-11 15:57:08 -0500

    adding equals and formatting to ValidationError

--- src/FubuValidation/Notification.cs
+++ src/FubuValidation/Notification.cs
@@ -23,6 +23,34 @@ namespace FubuValidation
 
         public string field { get; set; }
         public string message { get; set; }
+
+        public bool Equals(ValidationError other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(other.field, field) && Equals(other.message, message);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (ValidationError)) return false;
+            return Equals((ValidationError) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((field != null ? field.GetHashCode() : 0)*397) ^ (message != null ? message.GetHashCode() : 0);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("field: {0}, message: {1}", field, message);
+        }
     }
 
     [Serializable]

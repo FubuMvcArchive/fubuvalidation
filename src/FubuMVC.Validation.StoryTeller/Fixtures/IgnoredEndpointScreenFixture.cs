0e1ccaa409a94d0c85b88c1dbb8d11004cbaa50a
jmarnold <rncodepoet@gmail.com>
2013-01-05 16:26:42 -0700

    Fixing the ST suite

--- src/FubuMVC.Validation.StoryTeller/Fixtures/IgnoredEndpointScreenFixture.cs
+++ src/FubuMVC.Validation.StoryTeller/Fixtures/IgnoredEndpointScreenFixture.cs
@@ -1,7 +1,15 @@
-﻿namespace FubuMVC.Validation.StoryTeller.Fixtures
+﻿using OpenQA.Selenium;
+using StoryTeller.Engine;
+
+namespace FubuMVC.Validation.StoryTeller.Fixtures
 {
 	public class IgnoredEndpointScreenFixture : ValidationScreenFixture<IgnoredModel>
 	{
-		 
+        [FormatAs("The text on the screen is: {value}")]
+        [return: AliasAs("value")]
+		 public string VerifyTheText()
+		 {
+		     return Driver.FindElement(By.TagName("pre")).Text;
+		 }
 	}
 }

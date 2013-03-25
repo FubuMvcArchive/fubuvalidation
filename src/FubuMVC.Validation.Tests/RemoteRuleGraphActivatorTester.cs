92f623fd54d75689ee5ccd771e0975d8168f4014
jfebres <jaime.febres@gmail.com>
2013-03-25 15:17:42 -0500

    Support for nested remotes in input types: red.

--- src/FubuMVC.Validation.Tests/RemoteRuleGraphActivatorTester.cs
+++ src/FubuMVC.Validation.Tests/RemoteRuleGraphActivatorTester.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 using Bottles;
 using Bottles.Diagnostics;
@@ -46,12 +47,41 @@ namespace FubuMVC.Validation.Tests
         [Test]
         public void fills_the_rules_in_the_rule_graph()
         {
-            var rule = theRuleGraph.RulesFor(ReflectionHelper.GetAccessor<ActivatorTargetWithRemotes>(x => x.Username)).Single();
+            var rule = remotesFor<ActivatorTargetWithRemotes>(x => x.Username).Single();
             rule.Type.ShouldEqual(typeof(UniqueUsernameRule));
 
-            theRuleGraph.RulesFor(ReflectionHelper.GetAccessor<ActivatorTargetWithRemotes>(x => x.Name)).ShouldHaveCount(0);
-            theRuleGraph.RulesFor(ReflectionHelper.GetAccessor<ActivatorTargetNoRemotes>(x => x.Name)).ShouldHaveCount(0);
+            remotesFor<ActivatorTargetWithRemotes>(x => x.Name).ShouldHaveCount(0);
+            remotesFor<ActivatorTargetNoRemotes>(x => x.Name).ShouldHaveCount(0);
         }
+
+        [Test]
+        public void register_remote_rules_for_nested_properties()
+        {
+            remotesFor<Nested>(x => x.NestedDateProperty)
+                .ShouldHaveCount(1).All(x => x.Rule is RemoteDateRule).ShouldBeTrue();
+            remotesFor<Nested>(x => x.NestedIntProperty)
+                .ShouldHaveCount(1).All(x => x.Rule is RemoteIntRule).ShouldBeTrue();
+            remotesFor<Nested>(x => x.NestedStringProperty)
+                .ShouldHaveCount(1).All(x => x.Rule is RemoteStringRule).ShouldBeTrue();
+        }
+
+        [Test]
+        public void register_remote_rules_for_collection_closing_types()
+        {
+            remotesFor<Item>(x => x.ItemDateProperty)
+                .ShouldHaveCount(1).All(x => x.Rule is RemoteDateRule).ShouldBeTrue();
+            remotesFor<Item>(x => x.ItemIntProperty)
+                .ShouldHaveCount(1).All(x => x.Rule is RemoteIntRule).ShouldBeTrue();
+            remotesFor<Item>(x => x.ItemStringProperty)
+                .ShouldHaveCount(1).All(x => x.Rule is RemoteStringRule).ShouldBeTrue();
+        }
+
+        private IEnumerable<RemoteFieldRule> remotesFor<T>(Expression<Func<T, object>> expression)
+        {
+            return theRuleGraph.RulesFor(expression.ToAccessor());
+        }
+
+
 
         public class RemoteRuleGraphEndpoint
         {
@@ -61,6 +91,10 @@ namespace FubuMVC.Validation.Tests
             }
 
             public AjaxContinuation get_no_remotes(ActivatorTargetNoRemotes request)
+            {
+                throw new NotImplementedException();
+            }
+            public AjaxContinuation get_nested_remotes(ActivatorTargetWithNestedRemotes input)
             {
                 throw new NotImplementedException();
             }
@@ -80,6 +114,36 @@ namespace FubuMVC.Validation.Tests
             public string Name { get; set; }
         }
 
+        public class ActivatorTargetWithNestedRemotes
+        {
+            public string ModelStringProperty { get; set; }
+            public int ModelIntProperty { get; set; }
+            public DateTime ModelDateProperty { get; set; }
+            public Nested ModelNestedProperty { get; set; }
+        }
+
+        public class Nested
+        {
+            public List<Item> Items { get; set; }
+            [RemoteStringRule]
+            [MaximumStringLength(10)]
+            public string NestedStringProperty { get; set; }
+            [RemoteIntRule]
+            public int NestedIntProperty { get; set; }
+            [RemoteDateRule]
+            public DateTime NestedDateProperty { get; set; }
+            public Nested NestedNestedProperty { get; set; }
+        }
+
+        public class Item
+        {
+            [RemoteStringRule]
+            public string ItemStringProperty { get; set; }
+            [RemoteIntRule]
+            public int ItemIntProperty { get; set; }
+            [RemoteDateRule]
+            public DateTime ItemDateProperty { get; set; }
+        }
 
     }
 
@@ -98,6 +162,57 @@ namespace FubuMVC.Validation.Tests
         public override IEnumerable<IFieldValidationRule> RulesFor(PropertyInfo property)
         {
             yield return new UniqueUsernameRule();
+        }
+    }
+
+
+    public class RemoteStringRuleAttribute : FieldValidationAttribute
+    {
+        public override IEnumerable<IFieldValidationRule> RulesFor(PropertyInfo property)
+        {
+            yield return new RemoteStringRule();
+        }
+    }
+
+
+    public class RemoteStringRule : IRemoteFieldValidationRule
+    {
+        public void Validate(Accessor accessor, ValidationContext context)
+        {
+        }
+
+        public StringToken Token { get; set; }
+    }
+
+    public class RemoteIntRule : IRemoteFieldValidationRule
+    {
+        public void Validate(Accessor accessor, ValidationContext context)
+        {
+        }
+
+        public StringToken Token { get; set; }
+    }
+    public class RemoteIntRuleAttribute : FieldValidationAttribute
+    {
+        public override IEnumerable<IFieldValidationRule> RulesFor(PropertyInfo property)
+        {
+            yield return new RemoteIntRule();
+        }
+    }
+
+    public class RemoteDateRule : IRemoteFieldValidationRule
+    {
+        public void Validate(Accessor accessor, ValidationContext context)
+        {
+        }
+
+        public StringToken Token { get; set; }
+    }
+    public class RemoteDateRuleAttribute : FieldValidationAttribute
+    {
+        public override IEnumerable<IFieldValidationRule> RulesFor(PropertyInfo property)
+        {
+            yield return new RemoteDateRule();
         }
     }
 }

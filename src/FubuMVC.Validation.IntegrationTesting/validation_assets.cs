b41bf5365f4b4e063acdc1cdca876394d11961e1
jmarnold <rncodepoet@gmail.com>
2013-07-11 14:28:32 -0500

    Fixing the build

--- src/FubuMVC.Validation.IntegrationTesting/validation_assets.cs
+++ src/FubuMVC.Validation.IntegrationTesting/validation_assets.cs
@@ -11,42 +11,42 @@ namespace FubuMVC.Validation.IntegrationTesting
     [TestFixture]
     public class validation_assets
     {
-		[SetUp]
-		public void SetUp()
-		{
-			ServerHarness.Start();
-		}
+        [SetUp]
+        public void SetUp()
+        {
+            ServerHarness.Start();
+        }
 
-		[TearDown]
-		public void TearDown()
-		{
-			
-		}
+        [TearDown]
+        public void TearDown()
+        {
 
-		[Test]
-		public void fetches_fubuvalidation()
-		{
-			var scripts = new List<string>
-			{
-				"_content/scripts/jquery-1.8.2.min.js",
-				"_content/scripts/underscore-min.js",
-				"_content/scripts/fubuvalidation.localization.js",
-				"_content/scripts/jquery.continuations.js",
-				"_content/scripts/jquery.form.js",
-				"_content/scripts/ajax-diagnostics.js",
-				"_content/scripts/fubuvalidation.rules.js",
-				"_content/scripts/fubuvalidation.core.js",
-				"_content/scripts/jquery.continuations.forms.js",
-				"_content/scripts/fubuvalidation.ui.js",
-				"_content/scripts/fubuvalidation.plugin.js",
-				
-			};
+        }
 
-			ServerHarness.Endpoints.Get<AssetEndpoint>(x => x.get_fubuvalidation())
-			   .StatusCodeShouldBe(HttpStatusCode.OK)
-			   .ScriptNames()
-			   .ShouldHaveTheSameElementsAs(scripts);
-		}
+        [Test]
+        public void fetches_fubuvalidation()
+        {
+            var scripts = new List<string>
+            {
+                "_content/scripts/fubucontinuations.js",
+                "_content/scripts/jquery-1.8.2.min.js",
+                "_content/scripts/underscore-min.js",
+                "_content/scripts/fubucontinuations.diagnostics.js",
+                "_content/scripts/fubuvalidation.localization.js",
+                "_content/scripts/jquery.form.js",
+                "_content/scripts/fubucontinuations.jquery.js",
+                "_content/scripts/fubuvalidation.rules.js",
+                "_content/scripts/fubucontinuations.jquery.forms.js",
+                "_content/scripts/fubuvalidation.core.js",
+                "_content/scripts/fubuvalidation.ui.js",
+                "_content/scripts/fubuvalidation.plugin.js",
+            };
+
+            ServerHarness.Endpoints.Get<AssetEndpoint>(x => x.get_fubuvalidation())
+               .StatusCodeShouldBe(HttpStatusCode.OK)
+               .ScriptNames()
+               .ShouldHaveTheSameElementsAs(scripts);
+        }
     }
 
     public class AssetEndpoint

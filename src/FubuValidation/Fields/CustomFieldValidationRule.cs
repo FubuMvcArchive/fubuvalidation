00e372f40f92e222b5c97a81e5c39cf026d393cf
Andrew Kharlamov <andrew.kharlamov@extendhealth.com>
2013-08-08 07:12:42 -0600

    Added ability to create an inline validation rule.

---
+++ src/FubuValidation/Fields/CustomFieldValidationRule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FubuCore.Reflection;
+using FubuLocalization;
+
+namespace FubuValidation.Fields
+{
+    public class CustomFieldValidationRule<T> : IFieldValidationRule
+    {
+        private readonly Func<T, bool> theCondition;
+
+        public CustomFieldValidationRule(Func<T, bool> condition)
+        {
+            theCondition = condition;
+            Token = ValidationKeys.InvalidFormat;
+        }
+
+        public CustomFieldValidationRule(Func<T, bool> condition, StringToken token)
+        {
+            theCondition = condition;
+            Token = token ?? ValidationKeys.InvalidFormat;
+        }
+
+        public void Validate(Accessor accessor, ValidationContext context)
+        {
+            if (theCondition((T)context.Target)) return;
+
+            context.Notification.RegisterMessage(accessor, Token, new TemplateValue[0]);
+        }
+
+        public StringToken Token { get; set; }
+        public ValidationMode Mode { get; set; }
+    }
+}

49986909696188c926210007a1b32e0453cf4e71
Matthew Smith <mtscout6@gmail.com>
2014-02-18 11:33:07 -0700

    Fix Formatting

--- src/FubuMVC.Validation.Tests/UI/RenderingStrategyTester.cs
+++ src/FubuMVC.Validation.Tests/UI/RenderingStrategyTester.cs
@@ -7,38 +7,41 @@ using NUnit.Framework;
 
 namespace FubuMVC.Validation.Tests.UI
 {
-	[TestFixture]
-	public class RenderingStrategyTester
-	{
+    [TestFixture]
+    public class RenderingStrategyTester
+    {
         private FormRequest theRequest;
 
-		[SetUp]
-		public void SetUp()
-		{
-			theRequest = new FormRequest(new ChainSearch {Type = typeof (object)}, new object());
-			var theForm = new FormTag("test");
-			theRequest.ReplaceTag(theForm);
-		}
+        [SetUp]
+        public void SetUp()
+        {
+            theRequest = new FormRequest(new ChainSearch {Type = typeof (object)}, new object());
+            var theForm = new FormTag("test");
+            theRequest.ReplaceTag(theForm);
+        }
 
-		[Test]
-		public void summary_strategy_adds_the_validation_summary_attribute()
-		{
-			RenderingStrategies.Summary.Modify(theRequest);
-			theRequest.CurrentTag.ToString().ShouldEqual("<form method=\"post\" action=\"test\" data-validation-summary=\"true\">");
-		}
+        [Test]
+        public void summary_strategy_adds_the_validation_summary_attribute()
+        {
+            RenderingStrategies.Summary.Modify(theRequest);
+            theRequest.CurrentTag.ToString()
+                .ShouldEqual("<form method=\"post\" action=\"test\" data-validation-summary=\"true\">");
+        }
 
-		[Test]
-		public void highlight_strategy_adds_the_validation_highlight_attribute()
-		{
-			RenderingStrategies.Highlight.Modify(theRequest);
-			theRequest.CurrentTag.ToString().ShouldEqual("<form method=\"post\" action=\"test\" data-validation-highlight=\"true\">");
-		}
+        [Test]
+        public void highlight_strategy_adds_the_validation_highlight_attribute()
+        {
+            RenderingStrategies.Highlight.Modify(theRequest);
+            theRequest.CurrentTag.ToString()
+                .ShouldEqual("<form method=\"post\" action=\"test\" data-validation-highlight=\"true\">");
+        }
 
-		[Test]
-		public void inline_strategy_adds_the_validation_inline_attribute()
-		{
-			RenderingStrategies.Inline.Modify(theRequest);
-			theRequest.CurrentTag.ToString().ShouldEqual("<form method=\"post\" action=\"test\" data-validation-inline=\"true\">");
-		}
-	}
+        [Test]
+        public void inline_strategy_adds_the_validation_inline_attribute()
+        {
+            RenderingStrategies.Inline.Modify(theRequest);
+            theRequest.CurrentTag.ToString()
+                .ShouldEqual("<form method=\"post\" action=\"test\" data-validation-inline=\"true\">");
+        }
+    }
 }

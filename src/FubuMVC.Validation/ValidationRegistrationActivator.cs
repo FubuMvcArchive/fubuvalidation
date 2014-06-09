5d6f6caa831e694948f596f068b81c49dac76f36
jeremydmiller <jeremydmiller@yahoo.com>
2014-06-09 14:55:26 -0500

    fixed race condition w/ by collapsing the two activators

--- src/FubuMVC.Validation/ValidationRegistrationActivator.cs
+++ src/FubuMVC.Validation/ValidationRegistrationActivator.cs
@@ -4,10 +4,29 @@ using Bottles;
 using Bottles.Diagnostics;
 using FubuCore;
 using FubuMVC.Core.Registration;
+using FubuMVC.Validation.Remote;
 using FubuValidation;
 
 namespace FubuMVC.Validation
 {
+    public class CompoundActivator : IActivator
+    {
+        private readonly ValidationRegistrationActivator _registration;
+        private readonly RemoteRuleGraphActivator _remotes;
+
+        public CompoundActivator(ValidationRegistrationActivator registration, RemoteRuleGraphActivator remotes)
+        {
+            _registration = registration;
+            _remotes = remotes;
+        }
+
+        public void Activate(IEnumerable<IPackageInfo> packages, IPackageLog log)
+        {
+            _registration.Activate(packages, log);
+            _remotes.Activate(packages, log);
+        }
+    }
+
     public class ValidationRegistrationActivator : IActivator
     {
         private readonly ValidationGraph _graph;
